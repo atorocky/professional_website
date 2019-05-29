609e2f26c640e67083b8f5122b1faec770884c8c
samrene208 <samrene208@gmail.com>
2019-05-28 20:59:09 -0400

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+   get 'welcome/index' => 'welcome#index'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  root 'welcome#index
+end
